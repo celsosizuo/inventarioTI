509350e598e602b2ca468dd3f70066677cc99052
Celso Sizuo <celso.sizuo@artfix.com.br>
2020-03-04 09:04:16 -0300

    Implementação da associação da licença ao computador

--- Inventario.TIC/Form1.Designer.cs
+++ Inventario.TIC/Form1.Designer.cs
@@ -28,13 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.drpAtivoNovo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.drpNotaFiscal = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.drpSoftware = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drpAtivoNovo
+            // 
+            this.drpAtivoNovo.FormattingEnabled = true;
+            this.drpAtivoNovo.Location = new System.Drawing.Point(15, 25);
+            this.drpAtivoNovo.Name = "drpAtivoNovo";
+            this.drpAtivoNovo.Size = new System.Drawing.Size(175, 21);
+            this.drpAtivoNovo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ativo Novo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nota Fiscal";
+            // 
+            // drpNotaFiscal
+            // 
+            this.drpNotaFiscal.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.drpNotaFiscal.FormattingEnabled = true;
+            this.drpNotaFiscal.Location = new System.Drawing.Point(15, 88);
+            this.drpNotaFiscal.Name = "drpNotaFiscal";
+            this.drpNotaFiscal.Size = new System.Drawing.Size(175, 21);
+            this.drpNotaFiscal.TabIndex = 2;
+            this.drpNotaFiscal.SelectedIndexChanged += new System.EventHandler(this.drpNotaFiscal_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Software";
+            // 
+            // drpSoftware
+            // 
+            this.drpSoftware.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.drpSoftware.FormattingEnabled = true;
+            this.drpSoftware.Location = new System.Drawing.Point(196, 88);
+            this.drpSoftware.Name = "drpSoftware";
+            this.drpSoftware.Size = new System.Drawing.Size(684, 21);
+            this.drpSoftware.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Confirmar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(131, 286);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(55, 13);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "Resultado";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 302);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(972, 502);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.drpSoftware);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.drpNotaFiscal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drpAtivoNovo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox drpAtivoNovo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox drpNotaFiscal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox drpSoftware;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Inventario.TIC/Form1.cs
+++ Inventario.TIC/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace Inventario.TIC
 {
@@ -16,5 +19,181 @@ namespace Inventario.TIC
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.drpAtivoNovo.DataSource = this.GetComputadores();
+                this.drpAtivoNovo.DisplayMember = "ATIVONOVO";
+                this.drpAtivoNovo.ValueMember = "ID";
+
+                this.drpNotaFiscal.DataSource = this.GetNotaFiscal();
+                this.drpNotaFiscal.DisplayMember = "NUMNF";
+                this.drpNotaFiscal.ValueMember = "NUMNF";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void drpNotaFiscal_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (drpNotaFiscal.SelectedIndex != -1)
+                {
+                    this.drpSoftware.DataSource = this.GetLicencas(this.drpNotaFiscal.SelectedValue.ToString());
+                    this.drpSoftware.DisplayMember = "NOME";
+                    this.drpSoftware.ValueMember = "ID";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string con = Properties.Settings.Default.conSQL;
+
+                SqlCommand command = new SqlCommand
+                {
+                    Connection = new SqlConnection(con),
+                    CommandType = CommandType.Text,
+                    CommandText = @"INSERT COMPUTADORESLICENCAS VALUES (" + this.drpAtivoNovo.SelectedValue.ToString() + ", " + this.drpSoftware.SelectedValue.ToString() + ")",
+                };
+
+                command.Connection.Open();
+                command.ExecuteScalar();
+
+                MessageBox.Show("Inclusão efetuada com sucesso.", "", MessageBoxButtons.OK);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private DataTable GetComputadores()
+        {
+            try
+            {
+                string con = Properties.Settings.Default.conSQL;
+
+                SqlCommand command = new SqlCommand
+                {
+                    Connection = new SqlConnection(con),
+                    CommandType = CommandType.Text,
+                    CommandText = "SELECT ID, ATIVONOVO FROM COMPUTADORES ORDER BY ATIVONOVO",
+                };
+
+                command.Connection.Open();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    DataRow dr = dt.NewRow();
+                    dr[0] = 0;
+                    dr[1] = "";
+                    dt.Rows.InsertAt(dr, 0);
+                    return dt;
+                }
+                else
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private DataTable GetNotaFiscal()
+        {
+            try
+            {
+                string con = Properties.Settings.Default.conSQL;
+
+                SqlCommand command = new SqlCommand
+                {
+                    Connection = new SqlConnection(con),
+                    CommandType = CommandType.Text,
+                    CommandText = @"SELECT DISTINCT A.NUMNF
+                                    FROM NOTAFISCAL A
+                                    INNER JOIN LICENCAS B ON A.ID = B.NOTAFISCALID
+                                    INNER JOIN SOFTWARE C ON C.ID = B.SOFTWAREID
+                                    ORDER BY NUMNF",
+                };
+
+                command.Connection.Open();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    DataRow dr = dt.NewRow();
+                    dr[0] = "";
+                    dt.Rows.InsertAt(dr, 0);
+                    return dt;
+                }
+                else
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private DataTable GetLicencas(string numNF)
+        {
+            try
+            {
+                string con = Properties.Settings.Default.conSQL;
+
+                SqlCommand command = new SqlCommand
+                {
+                    Connection = new SqlConnection(con),
+                    CommandType = CommandType.Text,
+                    CommandText = @"SELECT B.ID, C.NOME + ' [ ' + B.CHAVE + ' ]' AS NOME
+                                    FROM NOTAFISCAL A
+                                    INNER JOIN LICENCAS B ON A.ID = B.NOTAFISCALID
+                                    INNER JOIN SOFTWARE C ON C.ID = B.SOFTWAREID
+                                    WHERE A.NUMNF = '" + numNF + "'" +
+                                    "ORDER BY NUMNF, C.NOME",
+                };
+
+                command.Connection.Open();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    DataRow dr = dt.NewRow();
+                    dr[0] = 0;
+                    dr[1] = "";
+                    dt.Rows.InsertAt(dr, 0);
+                    return dt;
+                }
+                else
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+       
     }
 }
